f2fb970523b0b78644b58320155be49100d98bda
Madison Vega <madison.r.vega@gmail.com>
2021-05-01 22:41:22 -0500

    added model relationships to index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,27 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+const LinkId = require('./LinkId');
 
 User.hasMany(Post, {
+    through: LinkId,
     foreignKey: 'post_id',
-})
+});
+
+Post.hasOne(User, {
+    through: LinkId,
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    through: LinkId,
+    foreignKey: 'user_id'
+
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment,
+    LinkId
+};
